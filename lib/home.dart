9bb02ea051d8d79cb2c26ad391c02567fd001d03
rin1004 <rin691004@naver.com>
2022-01-19 00:24:22 +0900

    first

--- lib/home.dart
+++ lib/home.dart
@@ -39,7 +39,7 @@ class _HomeState extends State<Home> {
                           onPressed: () {
                             getImage(ImageSource.camera);
                           },
-                          child: Text("PICK FROM CAMERA"),
+                          child: Text("PICK FROM CAMERA!"),
                         )
                       ],
                     ),
